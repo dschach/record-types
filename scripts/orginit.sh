7ea90024fce925819bcc9391b5168e44ac895cf2
David Schach <dschach@x2od.com>
2022-01-17 19:26:52 -0800

    Update orginit.sh
    Delete old org and add progress indicator

--- scripts/orginit.sh
+++ scripts/orginit.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+echo "Cleaning previous scratch org..."
+sfdx force:org:delete -p -u RecordTypes
+
+echo "Creating new scratch org"
 sfdx force:org:create -f config/project-scratch-def.json --durationdays 10 -a RecordTypes -s
 
+echo "Pushing metadata"
 sfdx force:source:push
 
+echo "Opening org"
 sfdx force:org:open
 
 echo "Org is set up"
