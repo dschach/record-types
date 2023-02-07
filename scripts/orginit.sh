5f718ab244b105a2815bbc769a017cf41dd90f09
David Schach <dschach@x2od.com>
2023-02-07 15:21:13 -0800

    API 56, new methods

--- scripts/orginit.sh
+++ scripts/orginit.sh
@@ -9,6 +9,12 @@ sfdx force:org:create -f config/project-scratch-def.json --durationdays 10 -a Re
 echo "Pushing metadata"
 sfdx force:source:push
 
+echo "Assigning permission set"
+sfdx force:user:permset:assign -n unpackaged/main/default/permissionsets/RecordTypes_DefaultRT_for_Testing
+
+echo "Running Apex tests"
+sfdx force:apex:test:run --codecoverage --resultformat human -l RunLocalTests --wait 20
+
 echo "Opening org"
 sfdx force:org:open
 
