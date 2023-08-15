bcf40ec8de28615d0f8c3680ea4babfe8d41c6f1
David Schach <dschach@x2od.com>
2023-08-15 16:37:32 -0700

    build(ApexDox): update ApexDox

--- scripts/updateHighlight.sh
+++ scripts/updateHighlight.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-curl -L "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js" -o "doc-assets/highlight.js"
-echo >> "assets/highlight.js"
-curl -L "https://cdn.jsdelivr.net/npm/highlightjs-apex/dist/apex.min.js" >> "doc-assets/highlight.js"
-
-#printf '<link href="assets/styling.css" rel="stylesheet" />' > "doc-assets/homePage.html"
-#echo >> "doc-assets/homePage.html"
-#npx marked -i README.md --gfm >> "doc-assets/homePage.html"
-
-#sed -i '' 's|href="./|target="_blank" href="https://github.com/dschach/salesforce-trigger-framework/tree/main/|g' doc-assets/homePage.html
-
-#npx prettier --write "doc-assets/homePage.html"
+curl -L "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11/build/highlight.min.js" -o "doc-assets/highlight.js"
+echo >> "doc-assets/highlight.js"
+curl -L "https://cdn.jsdelivr.net/npm/highlightjs-apex@1/dist/apex.min.js" >> "doc-assets/highlight.js"
