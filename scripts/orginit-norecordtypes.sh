d711a93c4b4f88448a3bf6c84c976fe5f2203c53
David Schach <dschach@x2od.com>
2023-08-18 20:51:55 -0700

    docs: update documentation

--- scripts/orginit-norecordtypes.sh
+++ scripts/orginit-norecordtypes.sh
@@ -4,14 +4,10 @@ echo "Cleaning previous scratch org..."
 sf org delete scratch --no-prompt --target-org RecordTypes
 
 echo "Creating new scratch org"
-sf org create scratch --definition-file config/project-scratch-def.json --duration-days 10 --alias RecordTypes --set-default
+sf org create scratch --definition-file config/no-rt-scratch-def.json --duration-days 10 --alias RecordTypes --set-default
 
 echo "Pushing metadata"
 sf project deploy start
-sf project deploy start --source-dir unpackaged
-
-echo "Assigning permission set"
-sf sf org assign permset --name RecordTypes_DefaultRT_for_Testing
 
 echo "Running Apex tests"
 sf apex run test --code-coverage --result-format human --test-level RunLocalTests --wait 20
