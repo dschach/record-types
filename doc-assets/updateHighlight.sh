7c90b047bcf3985dce9ac75e6943d4dda177ccd9
David Schach <dschach@x2od.com>
2024-02-12 13:13:36 -0800

    docs: update to 1.5.0.4 docs

---
+++ doc-assets/updateHighlight.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+FILE=node_modules/@highlightjs/cdn-assets/highlight.min.js
+if test -f "$FILE"; then
+	cat node_modules/@highlightjs/cdn-assets/highlight.min.js > doc-assets/highlight.js
+	echo "\n" >> doc-assets/highlight.js
+	cat node_modules/highlightjs-apex/dist/apex.min.js >> doc-assets/highlight.js
+else
+	curl -L "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release/build/highlight.min.js" -o "doc-assets/highlight.js"
+	echo >> "doc-assets/highlight.js"
+	curl -L "https://cdn.jsdelivr.net/npm/highlightjs-apex/dist/apex.min.js" >> "doc-assets/highlight.js"
+fi
