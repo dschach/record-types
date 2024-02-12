f3f590d50c97e1191d8c05b797907a0fd738cb0f
David Schach <dschach@x2od.com>
2024-02-12 12:07:55 -0800

    object has RT but user only has Master

--- scripts/packagedev/build.sh
+++ scripts/packagedev/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sf package version create --code-coverage --installation-key-bypass --wait 90 --verbose --definition-file config/project-scratch-def.json --package "Record Types" --target-dev-hub x2od
+sf package version create --code-coverage --installation-key-bypass --wait 90 --verbose --definition-file config/no-rt-scratch-def.json --package "Record Types" --target-dev-hub x2od
